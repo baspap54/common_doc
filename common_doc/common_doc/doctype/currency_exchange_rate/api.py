12d57ea9786c577171dfd48051655df695f690de
John Park <yuntan0@gmail.com>
2022-08-24 06:13:17 +0000

    Unlocode parsing

--- common_doc/common_doc/doctype/currency_exchange_rate/api.py
+++ common_doc/common_doc/doctype/currency_exchange_rate/api.py
@@ -8,6 +8,7 @@ import requests
 import json
 import os
 from bs4 import BeautifulSoup
+# from pyproj import Proj,transform
 import datetime
 
 def random_string(string_length=8):
@@ -274,26 +275,30 @@ def get_calendar(**kwargs):
 		##	print(key)
 		##	print(value)
 
-# bench execute common_doc.common_doc.doctype.currency_exchange_rate.api.get_unlocode_port --kwargs "{'country_cd':'KR','year':'2022'}"
+# bench execute common_doc.common_doc.doctype.currency_exchange_rate.api.get_unlocode_port --kwargs "{'country_cd':'kr'}"
 
 @frappe.whitelist()
 def get_unlocode_port(**kwargs):
-	print(kwargs)
-	url = 'https://service.unece.org/trade/locode/sg.htm'
+	# print(kwargs)
+	# proj_UTMK = Proj(init='epsg:5178')
+	# proj_WGS84 = Proj(init='epsg:4326')
+	country_cd = kwargs.get('country_cd')
+	url = 'https://service.unece.org/trade/locode/'+country_cd+'.htm'
 	res = requests.get(url)
 	html = res.content
 	soup = BeautifulSoup(html, 'lxml')
 	# list_tr = soup.find_all(name="tr", attrs={"class": "txtAr"})
+	
 	list_tr = soup.find_all(name="tr")
 	# print(len(list_tr))
 	for tr_el in list_tr:
 		list_td = tr_el.find_all(name='td',attrs={"height": "1","valign":"Top"})
-		if len(list_td)==11:
+		if len(list_td)==11 and list_td[0].get_text() != 'Ch' and list_td[1].text.strip()[4:7] != '':
 			port_doc = frappe.new_doc('Port Code')
-			port_doc.country_code = list_td[1].get_text()[0:2]
-			port_doc.port_code = list_td[1].get_text()[4:7]
+			port_doc.country_code = (list_td[1].get_text())[0:2]
+			port_doc.port_code = (list_td[1].get_text())[4:7]
 			port_doc.port_name = list_td[2].get_text()
-			port_doc.iata = list_td[8].get_text()
+			port_doc.iata = list_td[8].text.strip()
 			if list_td[5].get_text()[0:1] == '1':	
 				port_doc.sea_port = 1
 			else:
@@ -335,17 +340,48 @@ def get_unlocode_port(**kwargs):
 				port_doc.border_crossing = 0
 
 			port_doc.status = list_td[6].get_text()
-			port_doc.iata = list_td[8].get_text()
+			port_doc.iata = list_td[8].text.strip()
 
-			if list_td[6].get_text()[0.1] == 'A':
+			if list_td[6].get_text()[0:1] == 'A':
 				port_doc.use_yn ='Y'
+			else:
+				port_doc.use_yn ='N'
 			# port_doc.latitude
 			# port_doc.longitude
+			#3501N 12652E 3508N 12903E 			 [ ]
 			port_doc.description = list_td[10].get_text()
 			port_doc.country = frappe.db.get_value('Country',{'code':list_td[1].get_text()[0:2]},'country_name')
+			Latitude = 0.000
+			Longitude = 0.000
+			port_doc.coordinates = list_td[9].text.strip()
+			if list_td[9].text.strip() != '':
+				# print("["+list_td[9].get_text()+"]") 
+				Latitude_minute = int(list_td[9].get_text()[2:4])
+				Longitude_minute = int(list_td[9].get_text()[9:11])
+				Latitude_degree = int(list_td[9].get_text()[0:2])
+				Longitude_degree = int(list_td[9].get_text()[6:9])
+				Latitude =  Latitude_degree 
+				Longitude = Longitude_degree
+				if Latitude_minute >0 :
+					Latitude += Latitude_minute/60
+				if Longitude_minute >0 :
+					Longitude += Longitude_minute/60
+
+				
+				# print(list_td[1].get_text()[0:2]+list_td[1].get_text()[4:7]+list_td[6].get_text()+list_td[9].get_text())
+				# print("["+list_td[9].text.strip()+"]")  
+				# print(Latitude)
+				# print(Longitude)
+			port_doc.latitude = Latitude
+			port_doc.longitude = Longitude
 			
-			print(list_td[1].get_text()[0:2]+list_td[1].get_text()[4:7]) 
-			# SG  BKM 
+
+			port_doc.insert(
+				ignore_permissions=True, # ignore write permissions during insert
+				ignore_links=True, # ignore Link validation in the document
+				ignore_if_duplicate=True, # dont insert if DuplicateEntryError is thrown
+				ignore_mandatory=True 
+			) 
 		# for td_el in list_td:
 		# 	# print(td_el.get_text())
 		# 	print(td_el)
--- common_doc/common_doc/doctype/port_code/port_code.js
+++ common_doc/common_doc/doctype/port_code/port_code.js
@@ -3,15 +3,7 @@
 
 frappe.ui.form.on('Port Code', {
 	// refresh: function(frm) {
-	onload_post_render(frm) {
-		if (!frm.doc.port_code && frm.doc.latitude && frm.doc.longitude) {
-			frm.fields_dict.location.map.setView([frm.doc.latitude, frm.doc.longitude], 13);
-		}
-		else {
-			frm.doc.latitude = frm.fields_dict.location.map.getCenter()['lat'];
-			frm.doc.longitude = frm.fields_dict.location.map.getCenter()['lng'];
-		}
-	},
+
 
 	// }
 });
