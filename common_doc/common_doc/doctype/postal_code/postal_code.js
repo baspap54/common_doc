50a3dc1136f6851621a3cdbfab98c3ff2294d12a
John Park <yuntan0@gmail.com>
2022-11-18 09:51:45 +0900

    get sub division api update and postal code

--- common_doc/common_doc/doctype/postal_code/postal_code.js
+++ common_doc/common_doc/doctype/postal_code/postal_code.js
@@ -5,4 +5,13 @@ frappe.ui.form.on('Postal Code', {
 	// refresh: function(frm) {
 
 	// }
+	onload_post_render(frm) {
+		if (!frm.doc.location && frm.doc.latitude && frm.doc.longitude) {
+			frm.fields_dict.location.map.setView([frm.doc.latitude, frm.doc.longitude], 13);
+		}
+		else {
+			frm.doc.latitude = frm.fields_dict.location.map.getCenter()['lat'];
+			frm.doc.longitude = frm.fields_dict.location.map.getCenter()['lng'];
+		}
+	},
 });
